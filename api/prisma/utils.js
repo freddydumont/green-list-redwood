c439562d959c2b7479992f73f76a23a68a750035
Frederick Morin <frederickmorin18@gmail.com>
2020-03-22 15:59:38 -0400

    Add seed log utils

---
+++ api/prisma/utils.js
@@ -0,0 +1,35 @@
+const chalk = require('chalk')
+const log = console.log
+
+function start(table) {
+  log(chalk`
+{bold ${table}}... [{yellow started}]
+  `)
+}
+
+function end(table) {
+  log(chalk`
+{bold ${table}}... [{green done}]
+  `)
+}
+
+function found(table, name, data) {
+  log(chalk`
+[${table}] {inverse ${name}} {yellow was found} in the DB:
+{bold
+${JSON.stringify(data, null, 2)}}`)
+}
+
+function created(table, name, data) {
+  log(chalk`
+[${table}] {inverse ${name}} {green was created}:
+{bold
+${JSON.stringify(data, null, 2)}}`)
+}
+
+module.exports = {
+  start,
+  end,
+  found,
+  created,
+}
