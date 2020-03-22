50126f3b4116911a75e33dd0d18d16f9b6dfc147
Frederick Morin <frederickmorin18@gmail.com>
2020-03-22 15:59:58 -0400

    Seed Availabilities

--- api/prisma/seeds.js
+++ api/prisma/seeds.js
@@ -2,22 +2,47 @@
 const { PrismaClient } = require('@prisma/client')
 const dotenv = require('dotenv')
 
+const log = require('./utils')
+
 dotenv.config()
 const db = new PrismaClient()
+
+const availabilities = [
+  'seasonal',
+  'betweenCourses',
+  'courses',
+  'dayZero',
+  'remote',
+]
 
 async function main() {
   // Seed data is database data that needs to exist for your app to run.
   // Ideally this file should be idempotent: running it multiple times
   // will result in the same database state (usually by checking for the
-  // existence of a record before trying to create it). For example:
-  //
-  //   await photon.user.find({ email: admin@redwoodjs.com }).orCreate({
-  //     firstName: 'Admin',
-  //     lastName: 'Istrator',
-  //     email: 'admin@redwoodjs.com',
-  //   })
+  // existence of a record before trying to create it).
 
-  console.log('No data to seed. See api/prisma/seeds.js for info.')
+  log.start('Availabilities')
+  for (let name of availabilities) {
+    const data = await db.availability.findOne({
+      where: {
+        name,
+      },
+    })
+
+    if (data) {
+      log.found('Availability', name, data)
+    } else {
+      const created = await db.availability.create({
+        data: {
+          name,
+        },
+      })
+
+      log.created('Availability', name, created)
+    }
+  }
+
+  log.end('Availabilities')
 }
 
 main()
