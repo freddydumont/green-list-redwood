5898ebb7bd292a419c208727c6f97e8b2e4c1d68
Frederick Morin <frederickmorin18@gmail.com>
2020-03-23 10:09:20 -0400

    Add seedLanguage function

--- api/prisma/seeds.js
+++ api/prisma/seeds.js
@@ -15,12 +15,9 @@ const availabilities = [
   'remote',
 ]
 
-async function main() {
-  // Seed data is database data that needs to exist for your app to run.
-  // Ideally this file should be idempotent: running it multiple times
-  // will result in the same database state (usually by checking for the
-  // existence of a record before trying to create it).
+const languages = ['fr', 'en']
 
+async function seedAvailabillities() {
   log.start('Availabilities')
   for (let name of availabilities) {
     const data = await db.availability.findOne({
@@ -43,6 +40,41 @@ async function main() {
   }
 
   log.end('Availabilities')
+}
+
+async function seedLanguages() {
+  log.start('Languages')
+  for (let name of languages) {
+    const data = await db.language.findOne({
+      where: {
+        name,
+      },
+    })
+
+    if (data) {
+      log.found('Language', name, data)
+    } else {
+      const created = await db.language.create({
+        data: {
+          name,
+        },
+      })
+
+      log.created('Language', name, created)
+    }
+  }
+
+  log.end('Languages')
+}
+
+async function main() {
+  // Seed data is database data that needs to exist for your app to run.
+  // Ideally this file should be idempotent: running it multiple times
+  // will result in the same database state (usually by checking for the
+  // existence of a record before trying to create it).
+
+  await seedAvailabillities()
+  await seedLanguages()
 }
 
 main()
