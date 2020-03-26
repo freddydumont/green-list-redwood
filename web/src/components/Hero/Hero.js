b2839f7c4948b78a0eadc270dfdc7805b78090ab
Frederick Morin <frederickmorin18@gmail.com>
2020-03-25 22:46:25 -0400

    WIP: Convert Hero to theme-ui

--- web/src/components/Hero/Hero.js
+++ web/src/components/Hero/Hero.js
@@ -1,24 +1,54 @@
+/** @jsx jsx **/
+import { jsx, Box, Text, Button } from 'theme-ui'
+import { Link, routes } from '@redwoodjs/router'
+import { useState } from 'react'
+
 const Hero = () => {
+  const [open, setOpen] = useState(false)
+
   return (
     <section>
       <link
         rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/@tailwindcss/ui@latest/dist/tailwind-ui.min.css"
+        href="https://cdn.jsdelivr.net/npm/@tailwindcss/ui@latest/dist/tailwind-ui.css"
       ></link>
-      <div
-        x-data="{ open: false }"
-        className="relative bg-white overflow-hidden"
+      <Box
+        sx={{
+          position: 'relative',
+          bg: 'white',
+          overflow: 'hidden',
+        }}
       >
-        <div className="max-w-screen-xl mx-auto ">
-          <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-            <div className="pt-6 px-4 sm:px-6 lg:px-8">
+        <Box
+          sx={{
+            maxWidth: 'screen-xl',
+            mx: 'auto',
+          }}
+        >
+          <Box
+            sx={{
+              position: 'relative',
+              zIndex: 10,
+              pb: [8, 16, 20, 28, 32],
+              bg: 'white',
+              maxWidth: [null, null, null, '2xl'],
+              width: [null, null, null, 'full'],
+            }}
+          >
+            <Box
+              sx={{
+                pt: 6,
+                px: [4, 6, 8],
+              }}
+            >
               <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start">
                 <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
                   <div className="flex items-center justify-between w-full md:w-auto">
                     <a href="#">
+                      {/* Dhamma wheel icon from https://github.com/twitter/twemoji/ */}
                       <img
                         className="h-8 w-auto sm:h-10"
-                        src="/img/logos/workflow-mark-on-white.svg"
+                        src="/img/dhamma_wheel.svg"
                         alt=""
                       />
                     </a>
@@ -62,25 +92,13 @@ const Hero = () => {
                   </a>
                   <a
                     href="#"
-                    className="ml-8 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition duration-150 ease-in-out"
-                  >
-                    Marketplace
-                  </a>
-                  <a
-                    href="#"
-                    className="ml-8 font-medium text-gray-500 hover:text-gray-900 focus:outline-none focus:text-gray-900 transition duration-150 ease-in-out"
-                  >
-                    Company
-                  </a>
-                  <a
-                    href="#"
                     className="ml-8 font-medium text-indigo-600 hover:text-indigo-900 focus:outline-none focus:text-indigo-700 transition duration-150 ease-in-out"
                   >
                     Log in
                   </a>
                 </div>
               </nav>
-            </div>
+            </Box>
 
             <div
               {...{
@@ -141,18 +159,6 @@ const Hero = () => {
                     >
                       Features
                     </a>
-                    <a
-                      href="#"
-                      className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
-                    >
-                      Marketplace
-                    </a>
-                    <a
-                      href="#"
-                      className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
-                    >
-                      Company
-                    </a>
                   </div>
                   <div>
                     <a
@@ -166,52 +172,109 @@ const Hero = () => {
               </div>
             </div>
 
-            <div className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-              <div className="sm:text-center lg:text-left">
-                <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-                  Data to enrich your
-                  <br className="xl:hidden" />
-                  <span className="text-indigo-600">online business</span>
-                </h2>
-                <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                  qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
-                  occaecat fugiat aliqua.
-                </p>
-                <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                  <div className="rounded-md shadow">
-                    <a
-                      href="#"
-                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
-                    >
-                      Get started
-                    </a>
-                  </div>
-                  <div className="mt-3 sm:mt-0 sm:ml-3">
-                    <a
-                      href="#"
-                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
-                    >
-                      Live demo
-                    </a>
-                  </div>
+            <Box
+              sx={{
+                mt: [10, 12, 16, 20, 28],
+                mx: 'auto',
+                px: [4, 6, 8],
+                maxWidth: 'screen-xl',
+              }}
+            >
+              <div
+                sx={{
+                  textAlign: [null, 'center', null, 'left'],
+                }}
+              >
+                <Text
+                  as="h2"
+                  sx={{
+                    fontSize: ['4xl', '5xl', '6xl'],
+                    fontWeight: 'extrabold',
+                    letterSpacing: 'tight',
+                    lineHeight: [10, 'none'],
+                    color: 'gray.9',
+                  }}
+                >
+                  {'Give service at '}
+                  <br
+                    sx={{
+                      display: [null, null, null, null, 'none'],
+                    }}
+                  />
+                  <span sx={{ color: 'primary' }}>Dhamma Suttama</span>
+                </Text>
+                <Text
+                  as="p"
+                  sx={{
+                    mt: [3, 5],
+                    mx: [null, 'auto', null, 0],
+                    fontSize: ['base', 'lg', 'xl'],
+                    maxWidth: [null, 'xl'],
+                    color: 'gray.6',
+                  }}
+                >
+                  If you have specialized skills, or you&apos;re available for
+                  serving in the kitchen, Dhamma Suttama would like to know!
+                </Text>
+                <div
+                  sx={{
+                    mt: [5, 8],
+                    display: [null, 'flex'],
+                    justifyContent: [null, 'center', null, 'start'],
+                  }}
+                >
+                  <Button as={Link} to={routes.form()} href="#">
+                    Get started
+                  </Button>
+                  <Box
+                    sx={{
+                      mt: [3, 0],
+                      ml: [null, 3],
+                    }}
+                  >
+                    <Button as="a" href="#" variant="gray">
+                      Learn more
+                    </Button>
+                  </Box>
                 </div>
               </div>
-            </div>
+            </Box>
             <svg
-              className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+              sx={{
+                display: ['none', null, null, 'block'],
+                position: 'absolute',
+                right: 0,
+                top: 0,
+                bottom: 0,
+                height: 'full',
+                width: 48,
+                color: 'white',
+                transform: 'translate(50%)',
+              }}
               fill="currentColor"
               viewBox="0 0 100 100"
               preserveAspectRatio="none"
             >
               <polygon points="50,0 100,0 50,100 0,100" />
             </svg>
-          </div>
-        </div>
-        <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            position: [null, null, null, 'absolute'],
+            top: [null, null, null, 0],
+            bottom: [null, null, null, 0],
+            right: [null, null, null, 0],
+            width: [null, null, null, '1/2'],
+          }}
+        >
           <picture>
             <img
-              className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+              sx={{
+                height: [56, 72, 96, 'full'],
+                width: 'full',
+                objectFit: 'cover',
+              }}
               sizes="(max-width: 1400px) 100vw, 1400px"
               srcSet="
               img/43232942312_fa286f2f0d_k_n8c4zf_c_scale,w_200.jpg 200w,
@@ -233,11 +296,11 @@ const Hero = () => {
               img/43232942312_fa286f2f0d_k_n8c4zf_c_scale,w_1384.jpg 1384w,
               img/43232942312_fa286f2f0d_k_n8c4zf_c_scale,w_1400.jpg 1400w"
               src="43232942312_fa286f2f0d_k_n8c4zf_c_scale,w_1400.jpg"
-              alt=""
+              alt="étagère de coussins de méditation"
             />
           </picture>
-        </div>
-      </div>
+        </Box>
+      </Box>
     </section>
   )
 }
