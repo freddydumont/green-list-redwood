fba3477590560d27cdeb6b32ffeb38cdcbd06b8e
Frederick Morin <frederickmorin18@gmail.com>
2020-04-11 14:31:52 -0400

    Refactor skill selection to work with database ids

--- web/src/components/SkillsCell/SkillsCell.js
+++ web/src/components/SkillsCell/SkillsCell.js
@@ -1,6 +1,7 @@
 import { Label, Checkbox } from 'theme-ui'
 import { useFormContext, Controller } from 'react-hook-form'
 import { motion } from 'framer-motion'
+import { useTranslation } from 'react-i18next'
 
 import CellLoading from '../CellLoading/CellLoading'
 import CellFailure from '../CellFailure/CellFailure'
@@ -49,7 +50,6 @@ export const Success = ({ skillDomains }) => {
     <>
       <FormInputChoice
         type="checkbox"
-        label="Areas of interest"
         name="categories"
         options={skillDomains.map((domain) => ({
           label: domain.name,
@@ -57,7 +57,7 @@ export const Success = ({ skillDomains }) => {
         }))}
       />
 
-      <ConditionalCheckboxes />
+      <ConditionalCheckboxes data={skillDomains} />
 
       <FormField label="Other skills" name="other" as="textarea" />
 
@@ -75,42 +75,20 @@ export const Success = ({ skillDomains }) => {
   )
 }
 
-const skillsData = {
-  kitchen: [
-    'question 1',
-    'question 2',
-    'question 3',
-    'question 4',
-    'question 5',
-    'question 6',
-    'question 7',
-    'question 8',
-  ],
-  maintenance: [
-    'painting',
-    'plumbing',
-    'carpentry',
-    'electricity',
-    'architecture',
-    'etc',
-  ],
-  technology: [
-    'sysadmin',
-    'software development',
-    'tech support',
-    'networking',
-    'etc',
-  ],
-  accounting: ['accounting', 'bookkeeping', 'etc'],
-}
-
 /**
  * Skill selection checkboxes that appear when a category is selected
  * A `react-hook-form` Controller is used to register the checkbox group
  * as the component mounts. The checkboxes themselves are registered directly.
  */
-const ConditionalCheckboxes = () => {
+const ConditionalCheckboxes = ({ data }) => {
   const { watch, control, register } = useFormContext()
+  const { t } = useTranslation()
+
+  /** given an id, returns the category object */
+  const getCategoryById = (id) => {
+    const category = data.find((cat) => cat.id == id)
+    return category
+  }
 
   // when a category is selected, the component is rerendered
   // so we have an updated array of user-selected categories
@@ -118,39 +96,37 @@ const ConditionalCheckboxes = () => {
 
   // skillsData is then rendered as a checkbox group for each category
   if (selectedCategories) {
+    // i18next-extract-disable
     return (
       <>
-        {selectedCategories.map((category) => (
-          <Controller
-            as={FormInputChoiceBox}
-            key={category}
-            label={category}
-            control={control}
-            // this template string creates a skill object with each
-            // selected category as properties when the form is submitted
-            name={`skills.${category}`}
-            animate
-          >
-            {skillsData[category].map((skill) => (
-              <motion.div
-                variants={{
-                  hidden: { opacity: 0 },
-                  show: { opacity: 1 },
-                }}
-                key={skill}
-              >
-                <Label>
-                  <Checkbox
-                    // and a skill property under the category
-                    name={`skills[${category}][${skill}]`}
-                    ref={register}
-                  />
-                  {skill}
-                </Label>
-              </motion.div>
-            ))}
-          </Controller>
-        ))}
+        {selectedCategories.map((categoryId) => {
+          const category = getCategoryById(categoryId)
+          return (
+            <Controller
+              as={FormInputChoiceBox}
+              key={category.name}
+              label={t(`form:categories.${category.name}`)}
+              control={control}
+              name={category.name}
+              animate
+            >
+              {category.skills.map((skill) => (
+                <motion.div
+                  variants={{
+                    hidden: { opacity: 0 },
+                    show: { opacity: 1 },
+                  }}
+                  key={skill.name}
+                >
+                  <Label>
+                    <Checkbox name="skills" value={skill.id} ref={register} />
+                    {t(`form:skills.${skill.name}`)}
+                  </Label>
+                </motion.div>
+              ))}
+            </Controller>
+          )
+        })}
       </>
     )
   }
