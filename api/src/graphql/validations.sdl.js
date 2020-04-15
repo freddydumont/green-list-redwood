1405b95930707714a282794b0dba13b433c39493
Frederick Morin <frederickmorin18@gmail.com>
2020-04-15 17:09:26 -0400

    Add validations mutations to sdl

---
+++ api/src/graphql/validations.sdl.js
@@ -0,0 +1,37 @@
+export const schema = gql`
+  input UserInfoInput {
+    firstName: String!
+    lastName: String!
+    email: String!
+    dateOfBirth: String!
+    gender: Gender!
+    lang: Lang!
+    location: String!
+    phone: String
+    contactPreference: ContactPreference!
+  }
+
+  input UserSkills {
+    kitchen: [ID]
+    maintenance: [ID]
+    technology: [ID]
+    accounting: [ID]
+  }
+
+  input UserSkillsInput {
+    categories: [ID]
+    skills: UserSkills
+    other: String
+    consent: Boolean!
+  }
+
+  input UserAvailabilityInput {
+    availability: [ID!]!
+  }
+
+  type Mutation {
+    validateUserInfo(input: UserInfoInput!): Boolean!
+    validateUserSkills(input: UserSkillsInput!): Boolean!
+    validateUserAvailability(input: UserAvailabilityInput!): Boolean!
+  }
+`
